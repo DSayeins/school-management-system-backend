ae2735228fa0177c3ec6b08d33d05ca9bfbb1b92
Djibrilla Mohammed <djibrilla414@gmail.com>
2025-10-18 11:12:09 +0100

    Ajustement de l'algorithme de mises a jour

--- app/Console/Commands/UpdateCurrencyToInt.php
+++ app/Console/Commands/UpdateCurrencyToInt.php
@@ -3,8 +3,14 @@
     namespace App\Console\Commands;
 
     use App\Helpers\utitlities\Convert;
+    use App\Models\Configuration;
+    use App\Models\Payment;
+    use App\Models\Receipt;
     use App\Models\Registration;
+    use App\Models\ScholarshipFeed;
     use Illuminate\Console\Command;
+    use Illuminate\Support\Facades\DB;
+    use Throwable;
 
     class UpdateCurrencyToInt extends Command
     {
@@ -30,11 +36,47 @@
             $this->info('--- Début de la mises à jour ---');
 
             $registrations = Registration::all();
+            $payments = Payment::all();
+            $receipts = Receipt::all();
+            $scholarships = ScholarshipFeed::all();
+            $configs = Configuration::all();
 
-            foreach ($registrations as $registration) {
-                $registration->update([
-                    'scholarship' => Convert::currencyToInt($registration->scholarship),
-                ]);
+            try {
+                DB::transaction(function () use ($registrations, $payments, $receipts, $scholarships, $configs) {
+                    foreach ($registrations as $registration) {
+                        $registration->update([
+                            'scholarship' => Convert::currencyToInt($registration->scholarship),
+                        ]);
+                    }
+
+                    foreach ($payments as $payment) {
+                        $payment->update([
+                            'paid' => Convert::currencyToInt($payment->paid),
+                            'remain' => Convert::currencyToInt($payment->remain),
+                        ]);
+                    }
+
+                    foreach ($receipts as $receipt) {
+                        $receipt->update([
+                            'amount' => Convert::currencyToInt($receipt->amount)
+                        ]);
+                    }
+
+                    foreach ($scholarships as $scholarship) {
+                        $scholarship->update([
+                            'normal' => Convert::currencyToInt($scholarship->normal),
+                            'subvention' => Convert::currencyToInt($scholarship->subvention),
+                        ]);
+                    }
+
+                    foreach ($configs as $config) {
+                        $config->update([
+                            'registration_fees' => Convert::currencyToInt($config->registration_fees)
+                        ]);
+                    }
+                });
+            } catch (Throwable $e) {
+                $this->info("Exception: " . $e->getMessage());
             }
 
             $this->info('--- Fin de la mises a jour ---');
